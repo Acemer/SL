846a2b065ec7e8564419910f704995e4bdfb5beb
Тимофей <@Home>
2014-08-03 13:32:33 +0600

    Еще работа с классом еды.

--- main/java/ru/nordwest/nord/item/ItemFood_f.java
+++ main/java/ru/nordwest/nord/item/ItemFood_f.java
@@ -1,13 +1,30 @@
 package ru.nordwest.nord.item;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
-import net.minecraft.potion.PotionEffect;
+import net.minecraft.block.Block;
 
-public class ItemFood_f extends ItemFood 
+public class ItemFood extends ItemFood
 {
-	private String name;
-	public String texture;
-	private PotionEffect[] potion;
+
+    public ItemFood(int par1, int par2, float par3, boolean par4)
+    {
+        super(par1, par2, par3, par4); 
+        this.setAlwaysEdible();
+    }
 
     
+    public void onFoodEaten(ItemFood par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        super.onFoodEaten(par1ItemStack, par2World, par3EntityPlayer);
+        if (!par2World.isRemote)
+        { 
+            par3EntityPlayer.addPotionEffect(new PotionEffect(Potion.fireResistance.id, 100, 0));
+        }
+ 
+    }
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister iconRegister) {
+        this.itemIcon = iconRegister.registerIcon("OreSpawn:" + (this.getUnlocalizedName().substring(5)));
+    }
+    
+}
