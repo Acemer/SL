0e87fa4c8dfc32a53f6fd7bbcf1665fa85527bdc
Тимофей <@Home>
2014-08-10 18:36:58 +0600

    Немного овечек

--- main/java/ru/nordwest/nord/client/ClientProxy.java
+++ main/java/ru/nordwest/nord/client/ClientProxy.java
@@ -1,14 +1,18 @@
 package ru.nordwest.nord.client;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import ru.nordwest.nord.CommonProxy;
 import net.minecraftforge.client.MinecraftForgeClient;
+import ru.nordwest.nord.client.renders.BlockCandleRenderer;
 
 
 public class ClientProxy extends CommonProxy {
         
         @Override
         public void registerRenderers() {
+        	 RenderingRegistry.registerBlockHandler(new BlockCandleRenderer());
                 // This is for rendering entities and so forth later on
         }
+        
         
 }
